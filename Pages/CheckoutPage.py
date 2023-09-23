4f546d5a8b089c08b07b797c37d1c78a8c85900e
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2023-09-23 22:45:42 +0600

    Product_Order

---
+++ Pages/CheckoutPage.py
@@ -0,0 +1,10 @@
+from Pages.BasePage import BaseFather
+
+
+class CheckOut(BaseFather):
+
+    def __init__(self,driver):
+        super().__init__(driver)
+
+
+
---
+++ TestCases/test_checkout.py
@@ -0,0 +1,6 @@
+from TestCases.Distribue import Distribute
+from Pages.CheckoutPage import CheckOut
+
+
+class Test_CheckOut(Distribute):
+    pass
