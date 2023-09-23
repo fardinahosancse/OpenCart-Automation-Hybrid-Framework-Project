a87ceb51e3410f6b4bdc7a562fc93e5e8d75e6d4
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2023-09-23 10:20:09 +0600

    Simplified Assertion-Reduce Code Complexity

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -16,10 +16,33 @@ log = Logger(__name__, logging.INFO)
 # @pytest.mark.skip
 class Test_Login(Distribute):
 
-    @pytest.mark.parametrize("email,password", dataProvider.get_data("Login", "../Excel/Registration.xlsx"))
+    @pytest.mark.parametrize("email,password", dataProvider.get_data("Login-valid", "../Excel/Registration.xlsx"))
     def test_Login_Using_valid_data(self, email, password, get_browser):
         driver = get_browser
-        print("Test_Login_Started------")
+        print("test_Login_Using_valid_data_Started------")
+
+        # Declaration
+        home = OpenCart_Home_Page(driver)
+        login = OpenCart_Login_Page(driver)
+        Openart = OpenCart(driver)
+
+        # Get Title From Home to ensure we are in right page
+        title = Openart.get_page_title()
+
+        # Login Execution
+        if "Your Store" == title:
+            home.go_to_Login().do_Login(email, password)
+            # Check Login Success by Comparing we are in new page that welcome new user
+            Login_success_title = Openart.get_page_title()
+            print("My Account: ", Login_success_title)
+            Expected = "My Account"
+            assert Login_success_title == Expected, f"Login Failed,Current Page :{Login_success_title}"
+
+
+    @pytest.mark.parametrize("email,password", dataProvider.get_data("Login-invalid", "../Excel/Registration.xlsx"))
+    def test_Login_Using_invalid_data(self, email, password, get_browser):
+        driver = get_browser
+        print("test_Login_Using_invalid_data_Started------")
 
         # Declaration
         home = OpenCart_Home_Page(driver)
@@ -36,19 +59,13 @@ class Test_Login(Distribute):
             # Check Login Success by Comparing we are in new page that welcome new user
             Login_success_title = Openart.get_page_title()
             print("My Account: ", Login_success_title)
-            Expected = "My Account"
-            Unexpected = "Account Login"
+            Expected = "Account Login"
+            assert Login_success_title == Expected, f"Unexpected Login Occurs::{Login_success_title}"
 
-            if Login_success_title == Expected:
-                assert Login_success_title == Expected, "Login Failed:Try Use different Mail and password"
-                log.logger.info("Expected: Successful Login")
-            elif Login_success_title == Unexpected:
-                assert Login_success_title == Unexpected, "Login Successful"
-                log.logger.info("Expected:Successful Login,but it Stays in Login Page")
-
-    def test_Login_Using_no_data(self,get_browser):
+    @pytest.mark.parametrize("email,password", dataProvider.get_data("Login-valid", "../Excel/Registration.xlsx"))
+    def test_Login_leaving_email_address(self, email, password, get_browser):
         driver = get_browser
-        print("Test_Login_Started------")
+        print("test_Login_leaving_email_address_Started------")
 
         # Declaration
         home = OpenCart_Home_Page(driver)
@@ -61,17 +78,58 @@ class Test_Login(Distribute):
 
         # Login Execution
         if "Your Store" == title:
-            home.go_to_Login().do_Login("","")
+            home.go_to_Login().do_Login("", password)
             # Check Login Success by Comparing we are in new page that welcome new user
             Login_success_title = Openart.get_page_title()
             print("My Account: ", Login_success_title)
             Expected = "Account Login"
-            Unexpected = "My Account"
+            assert Login_success_title == Expected, f"Unexpected Login Occurs::{Login_success_title}"
+
+    @pytest.mark.parametrize("email,password", dataProvider.get_data("Login-valid", "../Excel/Registration.xlsx"))
+    def test_Login_leaving_password(self, email, password, get_browser):
+        driver = get_browser
+        print("test_Login_leaving_password_Started------")
+
+        # Declaration
+        home = OpenCart_Home_Page(driver)
+        login = OpenCart_Login_Page(driver)
+        Openart = OpenCart(driver)
+
+        # Get Title From Home to ensure we are in right page
+        title = Openart.get_page_title()
+        print("Page Title: ", title)
+
+        # Login Execution
+        if "Your Store" == title:
+            home.go_to_Login().do_Login(email, "")
+            # Check Login Success by Comparing we are in new page that welcome new user
+            Login_success_title = Openart.get_page_title()
+            print("My Account: ", Login_success_title)
+            Expected = "Account Login"
+            assert Login_success_title == Expected, f"Unexpected Login Occurs::{Login_success_title}"
+
+    def test_Login_Using_no_data(self, get_browser):
+        driver = get_browser
+        print("test_Login_Using_no_data_Started------")
+
+        # Declaration
+        home = OpenCart_Home_Page(driver)
+        login = OpenCart_Login_Page(driver)
+        Openart = OpenCart(driver)
+
+        # Get Title From Home to ensure we are in right page
+        title = Openart.get_page_title()
+        print("Page Title: ", title)
+
+        # Login Execution
+        if "Your Store" == title:
+            home.go_to_Login().do_Login("", "")
+            # Check Login Success by Comparing we are in new page that welcome new user
+            Login_success_title = Openart.get_page_title()
+            print("My Account: ", Login_success_title)
+            Expected = "Account Login"
 
 
-            if Login_success_title == Expected:
-                assert Login_success_title == Expected, "Login Successfull"
-                log.logger.info("Expected: Unsuccessful Login")
-            elif Login_success_title != Unexpected:
-                assert Login_success_title == Unexpected, "Login UnSuccessful"
-                log.logger.info("Expected:Unsuccessful Login,but It Logged In")
+            # Objective : Login Fail if Inputs are blank
+            assert Login_success_title == Expected, f"Login Occurs :{Login_success_title}"
+
--- TestCases/test_registration.py
+++ TestCases/test_registration.py
@@ -11,12 +11,11 @@ from Utility.LogUtil import Logger
 log = Logger(__name__, logging.INFO)
 
 
-@pytest.mark.skip
 class Test_Register(Distribute):
 
     @pytest.mark.parametrize("firstname,lastname,email,password",
-                             dataProvider.get_data("Registration", "../Excel/Registration.xlsx"))
-    def test_registration_with_valid_data(self, firstname, lastname, email, password, get_browser):
+                             dataProvider.get_data("Registration-valid", "../Excel/Registration.xlsx"))
+    def test_registration_with_valid_data(self, firstname, lastname, email, password,get_browser):
         driver = get_browser
         print("Test_registration_with_valid_data_Started------")
         home = OpenCart_Home_Page(driver)  # Use get_browser here
@@ -26,14 +25,28 @@ class Test_Register(Distribute):
             home.go_to_Register().Fill_Registration_Form(firstname, lastname, email, password)
             time.sleep(2)
             Registration_success_title = Openart.get_page_title()
+            Expected="Your Account Has Been Created!"
+            assert Registration_success_title == Expected, f"Registration Failed,Current Page {Registration_success_title}"
+        else:
+            print("Element Not Found")
 
-            # Logic for Handling Same Repeated Email
-            if Registration_success_title == "Your Account Has Been Created!":
-                log.logger.info("Expected: Successful Register")
-                assert Registration_success_title == "Your Account Has Been Created!", "Register Unsuccessful"
-            elif Registration_success_title == "Register Account":
-                log.logger.info("Expected: Unsuccessful Register")
-                assert Registration_success_title == "Register Account", "Register Successful"
+
+
+
+    @pytest.mark.parametrize("firstname,lastname,email,password",
+                             dataProvider.get_data("Registration-invalid", "../Excel/Registration.xlsx"))
+    def test_registration_with_repeated_mail(self, firstname, lastname, email, password,get_browser):
+        driver = get_browser
+        print("Test_registration_with_valid_data_Started------")
+        home = OpenCart_Home_Page(driver)  # Use get_browser here
+        Openart = OpenCart(driver)  # Use get_browser here
+        title = Openart.get_page_title()
+        if "Your Store" == title:
+            home.go_to_Register().Fill_Registration_Form(firstname, lastname, email, password)
+            time.sleep(2)
+            Registration_success_title = Openart.get_page_title()
+            Expected="Register Account"
+            assert Registration_success_title == Expected, f"Unexpected Registration Occured::{Registration_success_title}"
         else:
             print("Element Not Found")
 
@@ -48,23 +61,15 @@ class Test_Register(Distribute):
             home.go_to_Register().Fill_Registration_Form("", "", "", "")  # Registration Form Fill
             time.sleep(2)
             Registration_Page_Title = Openart.get_page_title()  # Check The current page tile to detect Register Successfull or not
-
             Expected_Result = "Register Account"
-            Unexpected_Result = "Your Account Has Been Created!"
+            assert Registration_Page_Title == Expected_Result, f"Unexpected Registration Occured::{Registration_Page_Title}"
 
-
-            if Registration_Page_Title == Expected_Result:
-                log.logger.info("Expected: Unsuccessful Register")
-                assert Registration_Page_Title == Expected_Result, "Register Successful"
-            elif Registration_Page_Title == Unexpected_Result:
-                log.logger.info("Expected: Unsuccessful Register:Bug")
-                assert Registration_Page_Title == Unexpected_Result, "Register Unsuccessful"
         else:
             print("Failed to Detect HomePage")
 
     @pytest.mark.parametrize("firstname,lastname,email,password",
-                             dataProvider.get_data("Registration", "../Excel/Registration.xlsx"))
-    def test_registration_leaving_privacy_agreement_toggle(self, firstname, lastname, email, password, get_browser):
+                             dataProvider.get_data("Registration-valid", "../Excel/Registration.xlsx"))
+    def test_registration_leaving_privacy_agreement_toggle(self,firstname, lastname, email, password,get_browser):
         driver = get_browser
         print("test_registration_leaving_privacy_agreement_toggle_Started------")
         home = OpenCart_Home_Page(driver)  # Use get_browser here
@@ -73,14 +78,10 @@ class Test_Register(Distribute):
         if "Your Store" == title:
             home.go_to_Register().Fill_Registration_Form_without_privacy_agreement(firstname, lastname, email, password)
             time.sleep(2)
-            Registration_success_title = Openart.get_page_title()
-
-            # Logic for Handling Same Repeated Email
-            if Registration_success_title == "Register Account":
-                log.logger.info("Expected: Unsuccessful Register")
-                assert Registration_success_title == "Register Account", "Register Successful"
-            elif Registration_success_title == "Your Account Has Been Created!":
-                log.logger.info("Unexpected: Successfully Register: Bug")
-                assert Registration_success_title == "Your Account Has Been Created!", "Register Unsuccessful"
+            Registration_Page_Title = Openart.get_page_title()  # Check The current page tile to detect Register Successfull or not
+            Expected_Result = "Register Account"
+            assert Registration_Page_Title == Expected_Result, f"Unexpected Registration Occured::{Registration_Page_Title}"
         else:
             print("Element Not Found")
+
+
